cfcbbbfbc67a91d150be9ddb337c78aee24fd9e5
Ivan <46654375+mrlldd@users.noreply.github.com>
2019-01-14 15:26:03 +0200

    asd

---
+++ Course work unity/Assets/script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    void Start()
+    {
+        for (int x = 1; x < 5; x++)
+        {
+            for (int y = 0; y <= x; y++)
+            {
+                Instantiate(gameObject, new Vector3(x, y, 0), Quaternion.identity);
+            }
+        }
+    }
+
+}
