4e633be98929fe460ad420b2e38807058eb9a8aa
Ivan <46654375+mrlldd@users.noreply.github.com>
2019-01-14 16:07:21 +0200

    asd

---
+++ Course work unity/Assets/Prefabs/PlaneSpCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneSpCube : MonoBehaviour
+{
+    int x = 0, y = 0, z = 0, limit;
+    void Start()
+    {
+        limit = 0;
+    }
+
+    void FixedUpdate()
+    {
+        if(limit !=5)
+        {
+            Instantiate(new GameObject("CubeCubeCube"), new Vector3(x, y, z), Quaternion.identity);
+            limit++;
+            x++;
+        }
+    }
+}
