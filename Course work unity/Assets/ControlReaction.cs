657249852a68eb1304daed5baaa10c25c0dfeee4
Ivan <46654375+mrlldd@users.noreply.github.com>
2019-01-15 22:52:33 +0200

    15.01.2019
    +buttons
    -some scripts

---
+++ Course work unity/Assets/ControlReaction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlReaction : MonoBehaviour
+{
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(controls.forward)
+        {
+            Debug.Log("w");
+        }
+        if(controls.back)
+        {
+            Debug.Log("s");
+        }
+        if(controls.rotLeft)
+        {
+            Debug.Log("a");
+        }
+        if(controls.rotRight)
+        {
+            Debug.Log("d");
+        }
+    }
+}
---
+++ Course work unity/Assets/controls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 
+ * control sets
+ */
+
+public class controls : MonoBehaviour
+{
+    public static bool forward, back, rotLeft, rotRight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        forward = Input.GetKeyDown(KeyCode.W);
+        back = Input.GetKeyDown(KeyCode.S);
+        rotLeft = Input.GetKeyDown(KeyCode.A);
+        rotRight = Input.GetKeyDown(KeyCode.D);
+    }
+}
