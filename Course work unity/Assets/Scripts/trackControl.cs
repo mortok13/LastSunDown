1d02cc32866f43683ead405ed3d6c7e390815be7
Ivan <46654375+mrlldd@users.noreply.github.com>
2019-02-06 14:03:30 +0200

    asd

--- Course work unity/Assets/Scripts/trackControl.cs
+++ Course work unity/Assets/Scripts/trackControl.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class trackControl : MonoBehaviour
 {
-    
-    [SerializeField]
-    private GameObject[] elements;
+    /************* BLOCKS *************/
+    // X x Y x Z
+    private GameObject Up1x1x1;
+    private GameObject Up2x1x1;
+    private GameObject Down1x1x1;
+    private GameObject Down2x1x1;
+    private GameObject Road1x1x1;
+    private GameObject Corner1x1x1;
+    /************* BLOCKS *************/
+    private GameObject[] visibleTrack = new GameObject[64];
     void Start()
     {
+
     }
 
     // Update is called once per frame
