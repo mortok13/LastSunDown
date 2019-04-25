d943386eaf5029db1d3d5481b04074658a484702
Ivan <46654375+mrlldd@users.noreply.github.com>
2019-04-25 22:39:39 +0300

    arch improvements

---
+++ Course work unity/Assets/Scripts/Classes/Block.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Block", menuName = "Block", order = 51)]
+public class Block : ScriptableObject
+{
+    public Vector2 sizeXZ;
+    public GameObject block;
+    public void RotateForY(float angle)
+    {
+        block.transform.Rotate(0, angle, 0);
+    }
+    Block()
+    {
+    }
+    public void Delete()
+    {
+        Destroy(this);
+    }
+}
